12091524a471853093be787e46ccfdcfeeebad8d
OwenMelbz <owenmelbz@gmail.com>
2016-01-02 13:40:33 +0000

    trying to defer the js

--- scripts/remote-assets.js
+++ scripts/remote-assets.js
@@ -1,4 +1,6 @@
 'use strict';
 
-//loadCSS('/assets/css/site.css');
-loadCSS('https://fonts.googleapis.com/css?family=Lato:400,300,700');
+setTimeout(function(){
+    loadCSS('/assets/css/site.css');
+    loadCSS('https://fonts.googleapis.com/css?family=Lato:400,300,700');
+},1);
